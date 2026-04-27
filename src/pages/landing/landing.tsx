ecd7bebf81becabd74321650e4e4e2a2d0153515
nafiz678 <nafizulikram3@gmail.com>
2026-04-27 14:11:13 +0600

    Update src/pages/landing/landing.tsx

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,8 +1,17 @@
+import BestDeals from './Best-Deals/best-deals';
+import Categories from './Categories/categories';
+import FeaturedSection from './Featured-section/featured-section';
+import Hero from './Hero/hero';
 
 export default function Landing() {
   return (
-    <div>
-      this is landing page
-    </div>
-  )
+    <main className="pt-6">
+      <div className="w-[70%] mx-auto">
+        <Hero />
+        <BestDeals />
+        <Categories />
+        <FeaturedSection/>
+      </div>
+    </main>
+  );
 }
