6da6b3c9fb1b86c2352628f41d41abb09de7647a
nafiz678 <nafizulikram3@gmail.com>
2026-04-27 18:12:07 +0600

    feat(layout): render shared footer in root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,13 +3,14 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "../index.css";
 import HotNews from "../components/shared/hot-news";
 import Heading from "../components/shared/heading";
+import Footer from "../components/shared/footer";
 
 const RootLayout = () => (
   <>
     <HotNews />
     <Heading />
     <Outlet />
-    <div className="min-h-screen"/>
+    <Footer />
     <TanStackRouterDevtools />
   </>
 );
