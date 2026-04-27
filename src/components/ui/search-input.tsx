27f7415525c4673a3bcb492b3f3bea6ec230cade
nafiz678 <nafizulikram3@gmail.com>
2026-04-27 14:11:14 +0600

    Add src/components/ui/search-input.tsx

---
+++ src/components/ui/search-input.tsx
@@ -0,0 +1,25 @@
+import { Search01Icon } from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react';
+
+export function SearchInput() {
+  return (
+    <div className="w-full max-w-130 rounded-xs bg-background px-5 py-2 shadow-sm">
+      <div className="flex items-center gap-3">
+        <input
+          name="search"
+          type="text"
+          placeholder="Search for anything..."
+          className="w-full bg-transparent text-xs text-slate-600 placeholder:text-slate-400 outline-none"
+        />
+
+        <HugeiconsIcon
+          icon={Search01Icon}
+          size={24}
+          strokeWidth={2.1}
+          className="shrink-0 text-foreground cursor-pointer"
+          aria-hidden="true"
+        />
+      </div>
+    </div>
+  );
+}
