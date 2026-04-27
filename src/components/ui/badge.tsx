cbdfc1a71f21e3b80d0ce0813a8f1735949455ad
nafiz678 <nafizulikram3@gmail.com>
2026-04-27 14:11:14 +0600

    Add src/components/ui/badge.tsx

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { cn } from '../../lib/utils';
+
+export type BadgeVariant =
+  | 'discount'
+  | 'hot'
+  | 'sold_out'
+  | 'default'
+  | 'sale'
+  | 'best_deals';
+
+type BadgeProps = {
+  children: React.ReactNode;
+  variant?: BadgeVariant;
+  className?: string;
+};
+
+const badgeStyles: Record<BadgeVariant, string> = {
+  discount: 'bg-yellow-400 text-neutral-900',
+  hot: 'bg-red-500 text-background',
+  sold_out: 'bg-slate-500 text-foreground',
+  default: 'bg-badge text-gray-800',
+  sale: 'bg-[#2DB224] text-background',
+  best_deals: 'bg-text-primary text-background',
+};
+
+export function Badge({
+  children,
+  variant = 'default',
+  className,
+}: BadgeProps) {
+  return (
+    <span
+      className={cn(
+        'inline-flex items-center justify-center rounded px-2 py-1 text-sm  uppercase tracking-wide',
+        badgeStyles[variant],
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
+}
