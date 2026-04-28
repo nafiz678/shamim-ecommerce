4b9868f2c0419a6ed1764aff87578270bcd2e70c
nafiz678 <nafizulikram3@gmail.com>
2026-04-28 21:38:07 +0600

    data(shop): add sidebar filter option definitions

---
+++ src/pages/shop_page/shop-data.ts
@@ -0,0 +1,57 @@
+
+export const categories: string[] = [
+  'Electronics Devices',
+  'Computer & Laptop',
+  'Computer Accessories',
+  'SmartPhone',
+  'Headphone',
+  'Mobile Accessories',
+  'Gaming Console',
+  'Camera & Photo',
+  'TV & Homes Appliances',
+  'Watches & Accessories',
+  'GPS & Navigation',
+  'Warable Technology',
+];
+
+export const priceRanges: string[] = [
+  'All Price',
+  'Under $20',
+  '$25 to $100',
+  '$100 to $300',
+  '$300 to $500',
+  '$500 to $1,000',
+  '$1,000 to $10,000',
+];
+
+export const brands: string[] = [
+  'Apple',
+  'Microsoft',
+  'Dell',
+  'Symphony',
+  'Sony',
+  'LG',
+  'One Plus',
+  'Google',
+  'Samsung',
+  'HP',
+  'Xiaomi',
+  'Panasonic',
+  'Intel',
+];
+
+export const tags: string[] = [
+  'Game',
+  'iPhone',
+  'TV',
+  'Asus Laptops',
+  'Macbook',
+  'SSD',
+  'Graphics Card',
+  'Power Bank',
+  'Smart TV',
+  'Speaker',
+  'Tablet',
+  'Microwave',
+  'Samsung',
+];
