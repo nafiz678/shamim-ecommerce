b585f105d12e1bd3cc158e9844da5d5c91af3699
nafiz678 <nafizulikram3@gmail.com>
2026-04-29 13:02:57 +0600

    refactor(computer-accessories): reuse shared product card component

--- src/pages/landing/Computer-accessories/computer-accessories.tsx
+++ src/pages/landing/Computer-accessories/computer-accessories.tsx
@@ -1,7 +1,7 @@
 import Button from '../../../components/ui/Button';
 import { ArrowRight02FreeIcons } from '@hugeicons/core-free-icons';
-import ProductsCard from '../Best-Deals/products-card';
 import type { ProductProps } from '../../../lib/type';
+import ProductsCard from '../../../components/ui/products-card';
 
 export default function ComputerAccessories({
   productList,
