74c6746fd44e65fffa7f36391094086efa506929
nafiz678 <nafizulikram3@gmail.com>
2026-05-03 22:32:53 +0600

    refactor(src\routes): update __root component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import "../index.css";
-import HotNews from "../components/shared/hot-news";
-import Heading from "../components/shared/heading";
-import Footer from "../components/shared/footer";
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import '../index.css';
+import HotNews from '../components/shared/hot-news';
+import Heading from '../components/shared/heading';
+import Footer from '../components/shared/footer';
 
 const RootLayout = () => (
   <>
