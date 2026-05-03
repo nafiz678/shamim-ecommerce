8a96140f7e0a040821b09626baeaa6599a7c5b70
nafiz678 <nafizulikram3@gmail.com>
2026-05-03 22:32:53 +0600

    refactor(src\pages\shop_page): update search-sort component

--- src/pages/shop_page/search-sort.tsx
+++ src/pages/shop_page/search-sort.tsx
@@ -17,37 +17,42 @@ export type SearchSortProp = {
   setSortBy: Dispatch<SetStateAction<SortBy>>;
 };
 
-export default function SearchAndSort({ query, setQuery, sortBy, setSortBy }: SearchSortProp) {
+export default function SearchAndSort({
+  query,
+  setQuery,
+  sortBy,
+  setSortBy,
+}: SearchSortProp) {
   return (
-    <div className="flex w-full items-center justify-between gap-6">
+    <div className="flex w-full items-center justify-between md:gap-6 gap-3">
       <div className="relative w-full max-w-100">
         <input
           type="search"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search for anything..."
-          className="w-full border border-border px-5.25 pr-16 text-[19px] font-normal text-[#22313a] shadow-[0_1px_2px_rgba(16,24,40,0.04)] outline-none transition-all duration-200 placeholder:text-[#7b8f9b] focus:border-[#c7d3da] focus:shadow-[0_0_0_4px_rgba(124,144,155,0.12)] rounded-xs py-1.5 placeholder:text-xs"
+          className="w-full rounded-xs border border-border md:px-5.25 px-3 md:pr-16 pr-4 py-3 md:text-xs text-xxs font-normal text-[#22313a] shadow-sm outline-none transition-all duration-200 placeholder:text-xs placeholder:text-[#7b8f9b] focus:border-[#c7d3da] focus:shadow-lg"
         />
 
         <HugeiconsIcon
           icon={Search}
-          className="absolute right-5.25 top-1/2 size-5 -translate-y-1/2 text-[#151f26] pointer-events-none"
+          className="absolute md:right-5.25 right-2 top-1/2 md:size-5 size-4 -translate-y-1/2 text-[#151f26] pointer-events-none"
           strokeWidth={2}
         />
       </div>
 
-      <div className="flex items-center gap-7">
+      <div className="flex items-center md:gap-7 gap-3">
         <span className="text-xs w-full font-medium text-foreground/80 text-nowrap">
           Sort by:
         </span>
 
-        <div className="flex items-center border border-[#dfe6ea] bg-white px-3 rounded-xs">
+        <div className="flex items-center  px-3 rounded-xs">
           <Dropdown
             label=""
             value={sortBy}
             options={sortOptions}
             onChange={(value) => setSortBy(value as SortBy)}
-            className="w-full text-xs text-foreground/70 text-nowrap py-2.5"
+            className="w-full text-xs border border-[#dfe6ea] bg-white  text-foreground/70 text-nowrap md:py-2.5 py-2"
           />
         </div>
       </div>
