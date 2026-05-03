a71eadfbbb6169d6edb726ee79efdf41f2bed020
nafiz678 <nafizulikram3@gmail.com>
2026-05-03 22:32:52 +0600

    refactor(src\pages\landing\Featured-section): update featured-section component

--- src/pages/landing/Featured-section/featured-section.tsx
+++ src/pages/landing/Featured-section/featured-section.tsx
@@ -10,11 +10,11 @@ export default function FeaturedSection({
 }) {
   return (
     <section className="pt-16">
-      <div className="grid grid-cols-12 gap-6">
+      <div className="grid grid-cols-12 md:gap-6 gap-2">
         {/* Left featured banner */}
-        <div className="col-span-12 lg:col-span-3">
-          <div className="relative h-full min-h-100 overflow-hidden rounded-sm bg-[#F2DC6D] px-6 pt-12 text-center space-y-2.5">
-            <p className="text-[10px] font-semibold uppercase tracking-wide text-[#BE4646]">
+        <div className="md:col-span-3 col-span-5">
+          <div className="relative h-full min-h-full overflow-hidden rounded-sm bg-[#F2DC6D] px-6 pt-12 text-center space-y-2.5">
+            <p className="text-xxs font-semibold uppercase tracking-wide text-[#BE4646]">
               Computer & Accessories
             </p>
 
@@ -27,9 +27,9 @@ export default function FeaturedSection({
             </p>
 
             <div className="mb-8 flex items-center justify-center gap-1">
-              <span className="text-[10px] font-medium ">Offers ends in:</span>
+              <span className="text-xxs font-medium ">Offers ends in:</span>
 
-              <span className="rounded-sm bg-background px-1 py-0.5 text-[10px] font-medium uppercase ">
+              <span className="rounded-sm bg-background px-1 py-0.5 text-xxs font-medium uppercase ">
                 Ends of Christmas
               </span>
             </div>
@@ -51,37 +51,36 @@ export default function FeaturedSection({
         </div>
 
         {/* Right products section */}
-        <div className="col-span-12 lg:col-span-9">
+        <div className="md:col-span-9 col-span-7">
           {/* Header */}
-          <div className="mb-6 flex items-center justify-between">
+          <div className="mb-6 flex md:flex-row flex-col h-20 items-center justify-between">
             <h2 className="text-lg text-nowrap font-semibold text-foreground">
               Featured Products
             </h2>
 
-            <div className="hidden items-center gap-3 lg:flex">
-              <button className="border-b-2 cursor-pointer border-secondary pb-1 px-1 font-semibold text-foreground text-xs text-nowrap">
-                All Product
-              </button>
-              <button className="text-foreground/40 text-xs pb-1 hover:text-foreground hover:border-b-2 cursor-pointer border-secondary ">
-                Smart Phone
-              </button>
-              <button className="text-foreground/40 text-xs pb-1 hover:text-foreground hover:border-b-2 cursor-pointer border-secondary ">
-                Laptop
-              </button>
-              <button className="text-foreground/40 text-xs pb-1 hover:text-foreground hover:border-b-2 cursor-pointer border-secondary ">
-                Headphone
-              </button>
-              <button className="text-foreground/40 text-xs pb-1 hover:text-foreground hover:border-b-2 cursor-pointer border-secondary ">
-                TV
-              </button>
-              <button className="pb-1 cursor-pointer font-medium text-secondary text-xs">
+            <div className="flex md:items-center items-end gap-3 md:flex-row flex-col">
+              <div className="flex items-center justify-center md:gap-3 gap-1">
+                {productTabs.map(({ label, active }) => (
+                  <button
+                    key={label}
+                    className={
+                      active
+                        ? 'border-b-2 cursor-pointer  border-secondary pb-1 px-1 font-semibold text-foreground md:text-xs sm:text-xxs text-[8px] text-nowrap'
+                        : 'text-foreground/40 md:text-xs sm:text-xxs text-[8px] pb-1 hover:text-foreground hover:border-b-2 cursor-pointer border-secondary text-nowrap'
+                    }
+                  >
+                    {label}
+                  </button>
+                ))}
+              </div>
+              <button className="md:pb-1 cursor-pointer font-medium text-secondary md:text-xs text-xxs">
                 Browse All Product →
               </button>
             </div>
           </div>
 
           {/* Product grid */}
-          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-4">
+          <div className="grid grid-cols-2 gap-5 lg:grid-cols-4">
             {productList.map((product) => (
               <ProductsCard featured key={product.id} product={product} />
             ))}
@@ -91,3 +90,26 @@ export default function FeaturedSection({
     </section>
   );
 }
+
+const productTabs: { label: string; active: boolean }[] = [
+  {
+    label: 'All Product',
+    active: true,
+  },
+  {
+    label: 'Smart Phone',
+    active: false,
+  },
+  {
+    label: 'Laptop',
+    active: false,
+  },
+  {
+    label: 'Headphone',
+    active: false,
+  },
+  {
+    label: 'TV',
+    active: false,
+  },
+];
