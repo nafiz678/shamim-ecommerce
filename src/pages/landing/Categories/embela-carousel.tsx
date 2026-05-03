d95a6ad1f644c6924790b548d133bbe5fd680ca5
nafiz678 <nafizulikram3@gmail.com>
2026-05-03 22:32:52 +0600

    refactor(src\pages\landing\Categories): update embela-carousel component

--- src/pages/landing/Categories/embela-carousel.tsx
+++ src/pages/landing/Categories/embela-carousel.tsx
@@ -1,7 +1,11 @@
 import type { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
 import type { CategoryProp } from '../../../lib/type';
-import { NextButton, PrevButton, usePrevNextButtons } from './embela-arrow-buttons';
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from './embela-arrow-buttons';
 
 type PropType = {
   slides: CategoryProp[];
@@ -25,24 +29,26 @@ const EmblaCarousel = (props: PropType) => {
         <div className="embla__container">
           {slides.map((slide, index) => (
             <div className="embla__slide" key={index}>
-              <div className="p-3">
+              <div className="md:p-3 p-1">
                 <div className="w-full flex flex-col items-center justify-center border border-border rounded pb-4">
                   <img
                     src={slide.image}
                     alt=""
-                    className="size-full object-contain p-4 rounded"
+                    className="size-full object-contain md:p-4 p-1.5 rounded"
                   />
-                  <h2 className="text-xs font-medium">{slide.title}</h2>
+                  <h2 className="md:text-xs text-xxs font-medium">
+                    {slide.title}
+                  </h2>
                 </div>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="absolute top-1/2 -translate-y-1/2 -left-5 text-background">
+      <div className="absolute top-[calc((100%/2)-10px)] -translate-y-1/2 left-0 md:-left-5 text-background">
         <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
       </div>
-      <div className="absolute top-1/2 -translate-y-1/2 -right-5 text-background">
+      <div className="absolute top-[calc((100%/2)-10px)] -translate-y-1/2 right-0 md:-right-5 text-background">
         <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
       </div>
     </div>
