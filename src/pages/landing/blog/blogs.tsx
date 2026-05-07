120825469d577c9b7ffe48a443789e821a8ec9b3
nafiz678 <nafizulikram3@gmail.com>
2026-05-08 00:55:16 +0600

    refactor: use lowercase button import in blogs

--- src/pages/landing/blog/blogs.tsx
+++ src/pages/landing/blog/blogs.tsx
@@ -4,7 +4,7 @@ import {
   UserCircleIcon,
 } from '@hugeicons/core-free-icons';
 import { HugeiconsIcon } from '@hugeicons/react';
-import Button from '../../../components/ui/Button';
+import Button from '../../../components/ui/button';
 
 export type PostProp = {
   author: string;
