b20320de6231749951d02da29dbdbb969ad646bb
nafiz678 <nafizulikram3@gmail.com>
2026-05-10 01:04:01 +0600

    feat: add mobile detection hook

---
+++ src/hooks/use-is-mobile.ts
@@ -0,0 +1,9 @@
+// hooks/use-is-mobile.ts
+
+import { useMediaQuery } from "./use-media-query";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  return useMediaQuery(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+}
