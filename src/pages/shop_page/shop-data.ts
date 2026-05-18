794d0fa27c5e2800383947d503378512427749cb
nafiz678 <nafizulikram3@gmail.com>
2026-05-18 15:42:14 +0600

    feat: define shop price presets

--- src/pages/shop_page/shop-data.ts
+++ src/pages/shop_page/shop-data.ts
@@ -1,57 +1,32 @@
-
-export const categories: string[] = [
-  'Electronics Devices',
-  'Computer & Laptop',
-  'Computer Accessories',
-  'SmartPhone',
-  'Headphone',
-  'Mobile Accessories',
-  'Gaming Console',
-  'Camera & Photo',
-  'TV & Homes Appliances',
-  'Watches & Accessories',
-  'GPS & Navigation',
-  'Warable Technology',
-];
-
-export const priceRanges: string[] = [
-  'All Price',
-  'Under $20',
-  '$25 to $100',
-  '$100 to $300',
-  '$300 to $500',
-  '$500 to $1,000',
-  '$1,000 to $10,000',
-];
-
-export const brands: string[] = [
-  'Apple',
-  'Microsoft',
-  'Dell',
-  'Symphony',
-  'Sony',
-  'LG',
-  'One Plus',
-  'Google',
-  'Samsung',
-  'HP',
-  'Xiaomi',
-  'Panasonic',
-  'Intel',
-];
-
-export const tags: string[] = [
-  'Game',
-  'iPhone',
-  'TV',
-  'Asus Laptops',
-  'Macbook',
-  'SSD',
-  'Graphics Card',
-  'Power Bank',
-  'Smart TV',
-  'Speaker',
-  'Tablet',
-  'Microwave',
-  'Samsung',
+export const pricePresets: { label: string; min: number; max: number }[] = [
+  {
+    label: "All Price",
+    min: 0,
+    max: 5000,
+  },
+  {
+    label: "$0 - $100",
+    min: 0,
+    max: 100,
+  },
+  {
+    label: "$100 - $500",
+    min: 100,
+    max: 500,
+  },
+  {
+    label: "$500 - $1000",
+    min: 500,
+    max: 1000,
+  },
+  {
+    label: "$1000 - $3000",
+    min: 1000,
+    max: 3000,
+  },
+  {
+    label: "$3000+",
+    min: 3000,
+    max: 5000,
+  },
 ];
