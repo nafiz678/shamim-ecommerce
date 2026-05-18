6a3e74806e04526689f8a61764f2438ebe1715e2
nafiz678 <nafizulikram3@gmail.com>
2026-05-18 16:13:57 +0600

    feat: load product details by shop slug

--- src/routes/(public)/shop/$slug.tsx
+++ src/routes/(public)/shop/$slug.tsx
@@ -1,10 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router";
-import BreadcrumbHeading, { type BreadcrumbItemProps } from "../../../pages/shop_page/breadcumb-heading";
+import {
+  createFileRoute,
+  notFound,
+  useLoaderData,
+} from "@tanstack/react-router";
+import BreadcrumbHeading, {
+  type BreadcrumbItemProps,
+} from "../../../pages/shop_page/breadcumb-heading";
 import { requirePrivateRoute } from "../../../lib/route-guards";
 import ProductsDetailsPage from "../../../pages/product_page/product-page";
 import ProductDetailsTabs from "../../../pages/product_page/products-description";
 import RelatedProducts from "../../../pages/product_page/related-products";
+import { apiFetch } from "../../../lib/api-fetch";
 
+type ApiResponse<T> = {
+  success: boolean;
+  message: string;
+  data: T;
+};
 
 export const Route = createFileRoute("/(public)/shop/$slug")({
   beforeLoad: async ({ location }) => {
@@ -12,14 +24,30 @@ export const Route = createFileRoute("/(public)/shop/$slug")({
       locationHref: location.href,
     });
   },
+  loader: async ({ params }) => {
+    const res = await apiFetch<ApiResponse<ProductDetailsProps>>(
+      `/products/${params.slug}`,
+    );
+
+    if (!res.success || !res.data) {
+      throw notFound();
+    }
+
+    return { product: res.data };
+  },
+
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { product } = useLoaderData({
+    from: "/(public)/shop/$slug",
+  });
+  console.log(product);
   return (
     <section>
       <BreadcrumbHeading items={breadcrumbs} />
-      <ProductsDetailsPage />
+      <ProductsDetailsPage product={product} />
       <ProductDetailsTabs />
       <RelatedProducts />
     </section>
@@ -40,3 +68,22 @@ const breadcrumbs: BreadcrumbItemProps[] = [
     active: true,
   },
 ];
+
+export type ProductDetailsProps = {
+  id: string;
+  title: string;
+  slug: string;
+  description: string | null;
+  price: number;
+  old_price: number | null;
+  images: string[] | null;
+  rating: number;
+  stock: number;
+  brand: string | null;
+  category?: {
+    id: string;
+    name: string;
+    slug: string;
+    image_url: string | null;
+  } | null;
+};
