8fb163d3fb8eae5da0efcb773415a6055a5c50b5
nafiz678 <nafizulikram3@gmail.com>
2026-06-03 14:01:49 +0600

    refactor: update admin layout

--- src/components/layout/admin-layout.tsx
+++ src/components/layout/admin-layout.tsx
@@ -17,7 +17,7 @@ import { HugeiconsIcon, type IconSvgElement } from "@hugeicons/react";
 import { cn } from "../../lib/utils";
 import { useIsMobile } from "../../hooks/use-is-mobile";
 import { useState } from "react";
-import { Input } from "../ui/input";
+import { Input } from "../../stories/Input/Input";
 
 type NavItem = {
   title: string;
