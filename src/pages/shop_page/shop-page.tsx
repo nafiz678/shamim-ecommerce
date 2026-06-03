05ba2f8fa2c0d7cc5b5daae7dde55126e6638444
nafiz678 <nafizulikram3@gmail.com>
2026-06-03 14:01:52 +0600

    refactor: update shop page

--- src/pages/shop_page/shop-page.tsx
+++ src/pages/shop_page/shop-page.tsx
@@ -17,7 +17,7 @@ import BreadcrumbHeading, {
 } from "./breadcumb-heading";
 import { apiFetch } from "../../lib/api-fetch";
 import { useQuery } from "@tanstack/react-query";
-import Button from "../../components/ui/button";
+import { Button } from "../../stories/Button";
 
 export type SortBy = "popular" | "price-low-high" | "price-high-low" | "rating";
 
